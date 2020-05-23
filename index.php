d8f841438fdcf3d19efeb65f34ef6f846c70be56
Mukul Tarania <43871224+mukultarania@users.noreply.github.com>
2020-05-23 21:14:57 +0530

    Include header and footer

--- index.php
+++ index.php
@@ -1,17 +1,4 @@
-<?php include_once 'include.php'; ?>
-<!doctype html>
-<html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
-
-    <title>Credential</title>
-  </head>
-  <body><br><br>
+<?php include_once 'header.php'; ?>
   <div class="container">
     <div class="row">
         <h2>Check Credential</h2>
@@ -34,8 +21,8 @@ if (isset($_POST['submit'])) {
     $credd = $_POST["credd"];
     if (empty($values)) {
         echo "No Data Found";
-    } else if(empty($credd)){
-      $b = 2;
+    } elseif (empty($credd)) {
+        $b = 2;
     } else {
         //$mask = "%10 %-10s %s\n";
         $b = 0;
@@ -62,7 +49,7 @@ if (isset($_POST['submit'])) {
         if ($b == 0) {
             echo "<li class='list-group-item'>No Record Found</li>";
         } else {
-          echo "";
+            echo "";
         }
         echo "</ul>";
     }
@@ -71,10 +58,4 @@ if (isset($_POST['submit'])) {
  </div>
 </div>
  </div>
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
-  </body>
-</html>
+<?php include_once 'footer.php'; ?>
