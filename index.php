ef7896b0695c805c585a4bf07a8c3c62b4a81c4a
Mukul Tarania <43871224+mukultarania@users.noreply.github.com>
2020-05-24 01:25:19 +0530

    Undefined Offset resolved
    Because sheet row have empty column

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php include_once 'header.php'; ?>
-  <div class="container">
-    <div class="row">
+<div class="container">
+  <div class="row">
         <h2>Check Credential</h2>
-    </div>
-    <div class="row justify-content-md-center">
-          <div class="col-8"><br>
+  </div>
+  <div class="row justify-content-md-center">
+    <div class="col-8"><br>
           <form action="index.php" method="POST">
           <div class="form-group">
             <label for="exampleInputEmail1">Enter Credential</label>
@@ -12,10 +12,10 @@
           </div>
           <button type="submit" class="btn btn-primary" name="submit">Submit</button>
           </form>
-          </div>
-      </div>
-    <div class="row justify-content-md-center">
-      <div class="col-10"><br>
+    </div>
+  </div>
+  <div class="row justify-content-md-center">
+    <div class="col-10"><br>
 <?php
 if (isset($_POST['submit'])) {
     $credd = $_POST["credd"];
@@ -28,6 +28,8 @@ if (isset($_POST['submit'])) {
         $b = 0;
         echo "<ul class='list-group'>";
         foreach ($values as $row) {
+          if(!empty($row[1]) && !empty($row[2]) && !empty($row[3]) && !empty($row[4]) && !empty($row[5]) && !empty($row[6]) && !empty($row[8])){
+            if (strcmp($credd, $row[3]) == 0) {
             $name = $row[2];
             $mail = $row[1];
             $ncs_id = $row[3];
@@ -35,7 +37,6 @@ if (isset($_POST['submit'])) {
             $ws_name = $row[5];
             $ws_date = $row[6];
             $link = $row[8];
-            if (strcmp($credd, $ncs_id) == 0) {
                 $b = 1;
                 echo "<li class='list-group-item'><strong style ='font-size: 25px;'>$name</strong>";
                 echo "<br /><strong>Email: </strong>$mail";
@@ -44,6 +45,7 @@ if (isset($_POST['submit'])) {
                 echo "<br /><strong>Workshop Name: </strong>".$ws_name."<br /><strong>Date: </strong>".$ws_date;
                 //echo "<li class='list-group-item'><small>Workshop Date:".$ws_date."</small></li>";
                 echo "<br /><a href= '$link'>$link</a></li>";
+              }
             }
         }
         if ($b == 0) {
@@ -55,7 +57,7 @@ if (isset($_POST['submit'])) {
     }
 }
  ?>
- </div>
+    </div>
+  </div>
 </div>
- </div>
 <?php include_once 'footer.php'; ?>
