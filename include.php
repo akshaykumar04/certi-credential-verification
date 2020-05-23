318901b6754f75dc831c43b032daf0937692b339
Mukul Tarania <43871224+mukultarania@users.noreply.github.com>
2020-05-23 20:35:08 +0530

    done

--- include.php
+++ include.php
@@ -3,7 +3,7 @@ require_once 'google-api-php-client-2.4.1\vendor\autoload.php';
 $client = new \Google_Client();
 $client->setApplicationName('GBUJobs');
 $client->setScopes([\Google_Service_Sheets::SPREADSHEETS]);
-$client->setAccessType('offline');
+$client->setAccessType('online');
 $client->setAuthConfig(__DIR__ . '/api.json');
 $service = new Google_Service_Sheets($client);
 $spreadsheetId = "1W89pRZ1FTBrc04st-h7nhk8iuPeIc85dBMr1byAV64U";
