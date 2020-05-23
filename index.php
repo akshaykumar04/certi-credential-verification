fdb0b1cb0a452553367f3e1c902844e65c2d4bd0
Akshay Kumar <mail.akshay04@gmail.com>
2020-05-24 01:32:41 +0530

    working on server

--- index.php
+++ index.php
@@ -1,4 +1,3 @@
-<?php include 'include.php'; ?>
 <!doctype html>
 <html lang="en">
   <head>
@@ -30,12 +29,24 @@
     <div class="row justify-content-md-center">
       <div class="col-10"><br>
 <?php
+require_once 'google-api-php-client-2.4.1/vendor/autoload.php';
+$client = new \Google_Client();
+$client->setApplicationName('GBUJobs');
+$client->setScopes([\Google_Service_Sheets::SPREADSHEETS]);
+$client->setAccessType('online');
+$client->setAuthConfig(__DIR__ . '/api.json');
+$service = new Google_Service_Sheets($client);
+$spreadsheetId = "1W89pRZ1FTBrc04st-h7nhk8iuPeIc85dBMr1byAV64U";
+$range = 'responses';
+$response = $service->spreadsheets_values->get($spreadsheetId, $range);
+$values = $response->getValues();
+
 if (isset($_POST['submit'])) {
     $credd = $_POST["credd"];
     if (empty($values)) {
         echo "No Data Found";
-    } else if(empty($credd)){
-      $b = 2;
+    } elseif (empty($credd)) {
+        $b = 2;
     } else {
         //$mask = "%10 %-10s %s\n";
         $b = 0;
@@ -47,7 +58,6 @@ if (isset($_POST['submit'])) {
             $grade = $row[4];
             $ws_name = $row[5];
             $ws_date = $row[6];
-            $cred = $row[2];
             $link = $row[8];
             if (strcmp($credd, $ncs_id) == 0) {
                 $b = 1;
@@ -56,14 +66,14 @@ if (isset($_POST['submit'])) {
                 echo "<br /><strong>NCS ID: </strong>".$ncs_id;
                 echo "<br /><strong>Grade: </strong>".$grade;
                 echo "<br /><strong>Workshop Name: </strong>".$ws_name."<br /><strong>Date: </strong>".$ws_date;
-                // echo "<li class='list-group-item'><small>Workshop Date:".$ws_date."</small></li>";
+                //echo "<li class='list-group-item'><small>Workshop Date:".$ws_date."</small></li>";
                 echo "<br /><a href= '$link'>$link</a></li>";
             }
         }
         if ($b == 0) {
             echo "<li class='list-group-item'>No Record Found</li>";
         } else {
-          echo "";
+            echo "";
         }
         echo "</ul>";
     }
@@ -72,10 +82,10 @@ if (isset($_POST['submit'])) {
  </div>
 </div>
  </div>
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
-  </body>
+<!-- Optional JavaScript -->
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+</body>
 </html>
